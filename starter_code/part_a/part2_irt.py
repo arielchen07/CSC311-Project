676f6eac07716196e48cc9db43bd0ac38c1178da
ruiting-chen <ruiting.chen.soc@gmail.com>
2022-12-02 15:16:18 -0500

    update part2_irt.py include c

--- starter_code/part_a/part2_irt.py
+++ starter_code/part_a/part2_irt.py
@@ -13,7 +13,7 @@ def sigmoid(x):
     return np.exp(x) / (1 + np.exp(x))
 
 
-def neg_log_likelihood(data, theta, beta, alpha):
+def neg_log_likelihood(data, theta, beta, alpha, c):
     """ Compute the negative log-likelihood.
 
     You may optionally replace the function arguments to receive a matrix.
@@ -35,6 +35,8 @@ def neg_log_likelihood(data, theta, beta, alpha):
         c_ij = data["is_correct"][i]
         alpha_theta_minus_beta = theta_minus_beta * alpha[data["question_id"][i]]
         sig = sigmoid(alpha_theta_minus_beta)
+        # uncomment the line below to add random guess parameter c
+        sig = c + (1 - c) * sig
         log_lklihood += c_ij * np.log(sig) + (1 - c_ij) * np.log(1 - sig)
 
     #####################################################################
@@ -43,7 +45,7 @@ def neg_log_likelihood(data, theta, beta, alpha):
     return -log_lklihood
 
 
-def update_theta_beta(data, lr, theta, beta, alpha):
+def update_theta_beta(data, lr, theta, beta, alpha, c):
     """ Update theta and beta using gradient descent.
 
     You are using alternating gradient descent. Your update should look:
@@ -59,6 +61,7 @@ def update_theta_beta(data, lr, theta, beta, alpha):
     :param theta: Vector
     :param beta: Vector
     :param alpha: Vector (how discriminative each question is)
+    :param c: parameter for random guess (probability of getting a question right by random guess)
     :return: tuple of vectors
     """
     #####################################################################
@@ -70,9 +73,12 @@ def update_theta_beta(data, lr, theta, beta, alpha):
     for i in range(len(data["is_correct"])):
         user_id = data["user_id"][i]
         question_id = data["question_id"][i]
-        partial_theta[user_id] += (data["is_correct"][i] -
-                                   sigmoid(alpha[question_id] * (theta[user_id] - beta[question_id]))) * \
-                                  alpha[question_id]
+        c_ij = data["is_correct"][i]
+        sig = sigmoid(alpha[question_id] * (theta[user_id] - beta[question_id]))
+        # partial_theta[user_id] += (c_ij - sig) * alpha[question_id]
+        # uncomment the line below and comment the line above to add random guess parameter c
+        partial_theta[user_id] += ((1 - c) * sig * (1 - sig) * alpha[question_id]) * \
+                                  (c_ij / (c + (1 - c) * sig) - (1 - c_ij) / (1 - c - (1 - c) * sig))
     theta += lr * partial_theta
 
     # fix theta, r, update beta
@@ -80,9 +86,12 @@ def update_theta_beta(data, lr, theta, beta, alpha):
     for i in range(len(data["is_correct"])):
         user_id = data["user_id"][i]
         question_id = data["question_id"][i]
-        partial_beta[question_id] += (- data["is_correct"][i] +
-                                   sigmoid(alpha[question_id] * (theta[user_id] - beta[question_id]))) * \
-                                  alpha[question_id]
+        c_ij = data["is_correct"][i]
+        sig = sigmoid(alpha[question_id] * (theta[user_id] - beta[question_id]))
+        # partial_beta[question_id] += (- c_ij + sig) * alpha[question_id]
+        # uncomment the line below and comment the line above to add random guess parameter c
+        partial_beta[question_id] += ((1 - c) * sig * (1 - sig) * alpha[question_id]) * \
+                                  ((- c_ij) / (c + (1 - c) * sig) + (1 - c_ij) / (1 - c - (1 - c) * sig))
     beta += lr * partial_beta
 
     # fix theta, beta, update r
@@ -90,9 +99,12 @@ def update_theta_beta(data, lr, theta, beta, alpha):
     for i in range(len(data["is_correct"])):
         user_id = data["user_id"][i]
         question_id = data["question_id"][i]
-        partial_alpha[question_id] += (data["is_correct"][i] -
-                                   sigmoid(alpha[question_id] * (theta[user_id] - beta[question_id]))) * \
-                                  (theta[user_id] - beta[question_id])
+        c_ij = data["is_correct"][i]
+        sig = sigmoid(alpha[question_id] * (theta[user_id] - beta[question_id]))
+        # partial_alpha[question_id] += (c_ij - sig) * (theta[user_id] - beta[question_id])
+        # uncomment the line below and comment the line above to add random guess parameter c
+        partial_alpha[question_id] += ((1 - c) * sig * (1 - sig) * (theta[user_id] - beta[question_id])) * \
+                                      (c_ij / (c + (1 - c) * sig) - (1 - c_ij) / (1 - c - (1 - c) * sig))
     alpha += lr * partial_alpha
     #####################################################################
     #                       END OF YOUR CODE                            #
@@ -100,7 +112,7 @@ def update_theta_beta(data, lr, theta, beta, alpha):
     return theta, beta, alpha
 
 
-def irt(data, val_data, lr, iterations):
+def irt(data, val_data, lr, iterations, c):
     """ Train IRT model.
 
     You may optionally replace the function arguments to receive a matrix.
@@ -123,20 +135,20 @@ def irt(data, val_data, lr, iterations):
     val_acc_lst = []
 
     for i in range(iterations):
-        neg_lld = neg_log_likelihood(data, theta=theta, beta=beta, alpha=alpha)
-        val_neg_lld = neg_log_likelihood(val_data, theta=theta, beta=beta, alpha=alpha)
-        score = evaluate(data=val_data, theta=theta, beta=beta, alpha=alpha)
+        neg_lld = neg_log_likelihood(data, theta=theta, beta=beta, alpha=alpha, c=c)
+        val_neg_lld = neg_log_likelihood(val_data, theta=theta, beta=beta, alpha=alpha, c=c)
+        score = evaluate(data=val_data, theta=theta, beta=beta, alpha=alpha, c=c)
         train_log_like.append(-neg_lld)
         val_log_like.append(-val_neg_lld)
         val_acc_lst.append(score)
         print("Iteration: {} \t NLLK: {} \t Score: {}".format(i, neg_lld, score))
-        theta, beta, r = update_theta_beta(data, lr, theta, beta, alpha)
+        theta, beta, r = update_theta_beta(data, lr, theta, beta, alpha, c)
 
     # TODO: You may change the return values to achieve what you want.
     return theta, beta, alpha, val_acc_lst, train_log_like, val_log_like
 
 
-def evaluate(data, theta, beta, alpha):
+def evaluate(data, theta, beta, alpha, c):
     """ Evaluate the model given data and return the accuracy.
     :param data: A dictionary {user_id: list, question_id: list,
     is_correct: list}
@@ -149,7 +161,9 @@ def evaluate(data, theta, beta, alpha):
     for i, q in enumerate(data["question_id"]):
         u = data["user_id"][i]
         x = (theta[u] - beta[q]).sum()
-        p_a = sigmoid(alpha[q] * x)
+        # p_a = sigmoid(alpha[q] * x)
+        # uncomment the line below to add random guess parameter c
+        p_a = c + (1 - c) * sigmoid(alpha[q] * x)
         pred.append(p_a >= 0.5)
     return np.sum((data["is_correct"] == np.array(pred))) \
            / len(data["is_correct"])
@@ -168,14 +182,15 @@ def main():
     # code, report the validation and test accuracy.                    #
     #####################################################################
     # hyperparameter:
-    num_iteration = 80
-    lr = 0.002
+    num_iteration = 120
+    lr = 0.0025
+    c = 0.25
 
-    theta, beta, alpha, acc, train_log_like, val_log_like = irt(train_data, val_data, lr, num_iteration)
+    theta, beta, alpha, acc, train_log_like, val_log_like = irt(train_data, val_data, lr, num_iteration, c)
     print("val accuracy: ")
-    print(evaluate(val_data, theta, beta, alpha))
+    print(evaluate(val_data, theta, beta, alpha, c))
     print("test accuracy: ")
-    print(evaluate(test_data, theta, beta, alpha))
+    print(evaluate(test_data, theta, beta, alpha, c))
 
     plt.plot([x for x in range(num_iteration)], train_log_like, label="train loglike")
     plt.xlabel("number of iterations")
